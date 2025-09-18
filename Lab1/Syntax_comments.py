b7a354e6ec77858fb64419d210966ae556aec3a6
Lyazzat1215 <lyazzat.onerkhan06@mail.ru>
2025-09-18 19:07:19 +0500

    Add files via upload

---
+++ Lab1/Syntax_comments.py
@@ -0,0 +1,8 @@
+if 5>2:
+    print ("five greater than two") # 4 отступа обычно автоматический идет а так можно любое колво 
+    print ("five greater than two") #оба принта доёлжны быть с одинаковыми отступами
+    """
+    multi comments
+    idk about it until rn 
+    """
+    
---
+++ Lab1/Var_names.py
@@ -0,0 +1,22 @@
+#allowed
+myvar = "lzzt"
+my_var = "bruh"
+_my_var = "kbtu"
+myVar = "discrete.s"
+MYVAR = "pythor"
+myvar2 = "snake"
+
+#doesnt allowed
+2myvar = "bleu" # впереди цыфра так низя
+my-var = "----" # тире ток как минус а так баг
+my var = "shoked" # no coment 
+
+#camel case- jnly firat word in ordinary letter
+myVarName="lzzt"
+
+#Pascal case- each word started to capital letter
+MyVarName="lzzt"
+
+#snake case- words separated by underscore
+my_var_name="lzzt"
+
