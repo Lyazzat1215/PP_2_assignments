1befe35ffedaeba7279db611b3c6a07f6a4cfcf6
Lyazzat1215 <lyazzat.onerkhan06@mail.ru>
2025-09-18 19:29:30 +0500

    Add files via upload

---
+++ Lab1/Mul-output_var.py
@@ -0,0 +1,28 @@
+#multiple var
+x, y, z = "Python", "C++", "C"
+print(x)
+print(y)
+print(z)
+#Make sure the number of variables matches the number of values, or else you will get an error.
+
+x = y = z = "python"
+print(x)
+print(y)
+print(z)
+# one value to all variables
+
+languages= ["C", "C++", "Python"] #unpacking list to each variable
+x, y, z = languages
+print(x)
+print(y)
+print(z)
+
+#output var
+x = "Pythor is awesome"
+print(x) # это и есть оутпут функция 
+# еще так можно
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z) #ну еще можно между xyz '+' mozhna НЕ ЗАБЫВАТЬ ПРО СПЕЙС
+# + еще может и как математический плюс работать нужно просто не мешать типы переменной
